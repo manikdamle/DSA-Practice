70c87f69f7aa2244719318d3f7504df9ffb03f9f
MD03 <115721290+manikdamle@users.noreply.github.com>
2024-10-23 13:47:58 +0530

    Add files via upload

---
+++ Find the Sum of Last N nodes of the Linked List.py
@@ -0,0 +1,73 @@
+'''
+Given a single linked list, calculate the sum of the last n nodes.
+
+Note: It is guaranteed that n <= number of nodes.
+
+Examples:
+
+Input: Linked List: 5->9->6->3->4->10, n = 3
+
+Output: 17
+Explanation: The sum of the last three nodes in the linked list is 3 + 4 + 10 = 17.
+'''
+
+#User function Template for python3
+
+'''
+# Node Class
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+'''
+class Solution:
+    def sumOfLastN_Nodes(self, head, n):
+        #function should return sum of last n nodes
+        p1=head
+        p2=head
+        for i in range(n):
+            p2=p2.next
+        x=True
+        while(p2 and p2.next!=None):
+            p1=p1.next
+            p2=p2.next
+            x=False
+        if x==False:
+            p1=p1.next
+        ans=0
+        while(p1.next!=None):
+            ans+=p1.data
+            p1=p1.next
+        ans+=p1.data
+        return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().split('\n')
+
+    t = int(data[0])
+    for i in range(1, t + 1):
+        arr = list(map(int, data[2 * i - 1].split()))
+        n = int(data[2 * i])
+        head = Node(arr[0])
+        tail = head
+        for value in arr[1:]:
+            tail.next = Node(value)
+            tail = tail.next
+        ob = Solution()
+        print(ob.sumOfLastN_Nodes(head, n))
+
+# } Driver Code Ends
